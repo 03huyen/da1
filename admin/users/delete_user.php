5261583e6e2edbe6627934fefa0bea628a064233
03huyen <117332456+03huyen@users.noreply.github.com>
2023-07-19 15:40:44 +0700

    login

--- admin/users/delete_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../../dao/users.php";
-$user_id = $_GET['user_id'];
-delete_user($user_id);
-setcookie('delete-product', "đã xóa khách hàng xong" , time() + 30);
-header("Location: ../index.php?act=users");
-die();
-
-?>
--- dao/categories.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "pdo.php";
-
-function categories_select_all(){
-    $sql = "SELECT * FROM categories;";
-    return pdo_query($sql);
-}
-?>
